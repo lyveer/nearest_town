7523a4a6e6cb8eec4b0d0426e80465ad1c76d856
lyver <114312666+lyveer@users.noreply.github.com>
2025-10-24 23:17:15 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,10 @@
-fx_version 'cerulean'   -- manifest sürümü
-game 'rdr3'             -- RedM (RDR2) için
+fx_version 'cerulean'   
+game 'rdr3'             
 
-name 'nearest_town'       -- kaynak adı (bilgi amaçlı)
+name 'nearest_town' 
 author 'lyver'
 version '1.0.0'
 
-client_scripts { 'client.lua' }  -- oyuncu tarafı kodu
+client_scripts { 'client.lua' }  
 
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
